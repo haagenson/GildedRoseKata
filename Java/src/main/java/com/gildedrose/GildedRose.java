5984608ab5c0c7955adc086407187886a83ba706
Tim Haagenson <Timothy.Haagenson@aa.com>
2021-04-01 13:48:37 -0500

    remove some duplication when increasing quality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -22,42 +22,32 @@ class GildedRose {
                     item.quality = 4;
                 }
             } else if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                item.quality = increaseQuality(item);
 
                 if (item.sellIn < 0) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    item.quality = increaseQuality(item);
                 }
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                item.quality = increaseQuality(item);
 
-                    if (item.sellIn < 10) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+                if (item.sellIn < 10) {
+                    item.quality = increaseQuality(item);
+                }
 
-                    if (item.sellIn < 5) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+                if (item.sellIn < 5) {
+                    item.quality = increaseQuality(item);
                 }
 
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 }
             } else {
-                item.quality = calculateQuality(item);
+                item.quality = decreaseQuality(item);
             }
         }
     }
 
-    private int calculateQuality(Item item) {
+    private int decreaseQuality(Item item) {
         int newQuality = item.quality;
         if (newQuality > 0) {
             newQuality = newQuality - 1;
@@ -69,6 +59,14 @@ class GildedRose {
             }
         }
 
+        return newQuality;
+    }
+
+    private int increaseQuality(Item item) {
+        int newQuality = item.quality;
+        if (newQuality < 50) {
+            newQuality = newQuality + 1;
+        }
         return newQuality;
     }
 }
