343dff5749b0f105ca3b36c361bb3478998920a0
Tim Haagenson <Timothy.Haagenson@aa.com>
2021-04-01 12:43:56 -0500

    Strangler pattern for legendary items

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,13 +15,13 @@ class GildedRose {
                 } else {
                     item.quality = 4;
                 }
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
             } else {
                 if (!item.name.equals("Aged Brie")
                         && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
                     }
                 } else {
                     if (item.quality < 50) {
@@ -43,17 +43,13 @@ class GildedRose {
                     }
                 }
 
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.sellIn = item.sellIn - 1;
-                }
+                item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
                     if (!item.name.equals("Aged Brie")) {
                         if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                             if (item.quality > 0) {
-                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    item.quality = item.quality - 1;
-                                }
+                                item.quality = item.quality - 1;
                             }
                         } else {
                             item.quality = 0;
